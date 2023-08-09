cfef72a6e14808a785a8db914f887210023a8119
Daggiana Madrid Andrade <daggianama@gmail.com>
2023-08-09 14:10:23 +0200

    test search

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import { useState } from 'react'
 import './App.css'
+import { AudioOutlined } from '@ant-design/icons';
+
+import { Input, Space } from 'antd';
+
+const { Search } = Input;
+
+const suffix = (
+  <AudioOutlined
+    style={{
+      fontSize: 16,
+      color: '#1677ff',
+    }}
+  />
+);
+
+const onSearch = (value: string) => console.log(value);
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-       
+      <Search placeholder="input search text" onSearch={onSearch} enterButton />
       </div>
 
     </>
