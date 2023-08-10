8c4742825232a4b94749b492d3c02beb4e0eccf4
Daggiana Madrid Andrade <daggianama@gmail.com>
2023-08-10 14:42:35 +0200

    basic pokemon detail api

--- src/components/PokemonDisplay.tsx
+++ src/components/PokemonDisplay.tsx
@@ -1,7 +1,41 @@
 import React, { useState, useEffect } from "react";
 
+interface Pokemon {
+    species: {
+        name: string;
+    };
+    sprites: {
+        other: {
+            dream_world: {
+                front_default: string;
+            };
+        };
+    };
+    id: number;
+    height: number;
+    weight: number;
+    abilities: {
+        ability: {
+            name: string;
+        };
+    }[];
+    stats: {
+        stat: {
+            name: string;
+        };
+        base_stat: number;
+    }[];
+    types: {
+        type: {
+            name: string;
+        };
+    }[];
+}
+
+
+
 export const PokemonDisplay = () => {
-    const [pokemon, setPokemon] = useState<any>(null);
+    const [pokemon, setPokemon] = useState <Pokemon | null> (null);
 
     const getPokemon = async () => {
         try {
@@ -21,7 +55,7 @@ export const PokemonDisplay = () => {
     return (
         <div>
             {pokemon && (
-                <div>
+                <div className="pokemon-card">
                     <div className="pokemon-header">
                     <h1>{pokemon.species.name.toUpperCase()}</h1>
                     <img src={pokemon.sprites.other.dream_world.front_default } alt="pokemon" />
@@ -36,13 +70,21 @@ export const PokemonDisplay = () => {
                         <h2>
                             Weight: {pokemon.weight}
                         </h2>
-                        {/* <h2>
-                            Abilities: {pokemon.abilities}
+                         <h2>
+                            Abilities: {pokemon.abilities.map((ability) => ability.ability.name).join(", ")}
                         </h2>
                         <h2>
-                            Type: {pokemon.type}
+                            Type: {pokemon.types.map((type) => type.type.name).join(", ")}
                         </h2>
-                  */}
+                       
+                        {pokemon.stats.map((stat) => 
+                            
+                            <h2> {stat.stat.name}: {stat.base_stat} </h2>
+                           
+                        )}
+                        
+
+                  
                      </div>
                 </div>)
                
